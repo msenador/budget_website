bb4598c622dfe28ee87cfa023f7d88b3fbc910e5
Morian Senador <smorian@yahoo.com>
2021-08-06 18:54:28 -0400

    wip

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -13,18 +13,40 @@ const CategoryCards = styled.div`
 `;
 
 function Categories({ val, userId }) {
+  const [updateCategory, setUpdateCategory] = useState('');
 
   const handleDelete = () => {
     const deleteRef = fire.database().ref(`${userId}/categories`).child(val.id)
     deleteRef.remove();
   }
 
+  const handleEdit = () => {
+    const postData ={
+      newCategory: updateCategory,
+    };
+
+    const editCategory = fire.database().ref(`${userId}/categories`).child(val).push().key;
+
+    const updates = {};
+    updates[editCategory] = updateCategory;
+
+    return fire.database().ref().update(updates);
+  }
+
+  useEffect(() => {
+    setUpdateCategory(updateCategory)
+  },[updateCategory]);
+
+
   return (
     <>
-    
     <CategoryCards>
     <div onClick={handleDelete}><DeleteIcon/></div>
-    <div><EditIcon/></div>
+    <div>
+      <EditIcon/>
+      <input placeholder="Edit" value={updateCategory} onChange={(e) => setUpdateCategory(e.target.value)}/>
+      <button onClick={handleEdit}>Update</button>
+    </div>
       {val.newCategory}
     </CategoryCards>
     </>
