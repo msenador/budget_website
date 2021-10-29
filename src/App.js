38813724207bb88ab7a0d15f7191c69d3085902d
Morian Senador <smorian@yahoo.com>
2021-10-29 17:04:58 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,126 +1,203 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import fire from './fire';
-import SignInOrSignUp from './components/SignIn';
-import HomePage from './components/HomePage';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import fire from "./fire";
+import SignInOrSignUp from "./components/SignIn";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import About from "./components/About";
+import HomePage from "./components/HomePage";
+import Contact from "./components/Contact";
+import { Icon } from "@iconify/react";
+import Login from "./components/Login";
 
-const SignInPageStyles = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #20bf55;
-  background-image: linear-gradient(315deg, #20bf55 0%, #01baef 74%);
-  overflow-y: auto;
+const Logo = styled.img`
+  src: "budgetstashlogo.png";
+`;
+
+const NavbarLogo = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: -50px;
+`;
+
+const NavbarLinks = styled.div`
+  padding-top: 10px;
+  height: 35px;
+  display: flex;
+  justify-content: space-around;
+  background-color: #78c091;
+  width: 100%;
 `;
 
 const App = () => {
-  const [userId, setUserId] = useState('');
-  const [user, setUser] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [hasAccount, setHasAccount] = useState(false);
-
-  const clearInputs = () => {
-    setEmail('');
-    setPassword('');
-  };
-
-  const clearErrors = () => {
-    setEmailError('');
-    setPasswordError('');
-  };
-
-  const handleLogin = () => {
-    clearErrors();
-    fire
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch((err) => {
-        switch (err.code) {
-          case 'auth/invalid-email':
-            setEmailError('Invalid Email');
-            break;
-          case 'auth/user-not-found':
-            setEmailError('User not found');
-            break;
-          case 'auth/wrong-password':
-            setPasswordError('Wrong password');
-            break;
-        }
-      });
-  };
-
-  const handleSignUp = () => {
-    clearErrors();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch((err) => {
-        switch (err.code) {
-          case 'auth/email-already-in-use':
-            setEmailError(err.message);
-            break;
-          case 'auth/invalid-email':
-            setEmailError('Invalid Email');
-            break;
-          case 'auth/weak-password':
-            setPasswordError(err.message);
-            break;
-        }
-      });
-  };
-
-  const handleLogout = () => {
-    fire.auth().signOut();
-  };
-
-  const authListener = () => {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        clearInputs();
-        setUserId(user.uid);
-        setUser(user);
-      } else {
-        setUser('');
-      }
-    });
-  };
-
-  useEffect(() => {
-    authListener();
-  }, []);
+  // const [user, setUser] = useState(false)
 
   return (
-    <>
-      <div className="App">
-        {user ? (
-          <HomePage
-          userId={userId}
-          handleLogout={handleLogout}
-          />
-        ) : (
-          <SignInPageStyles>
-            <SignInOrSignUp
-              email={email}
-              setEmail={setEmail}
-              password={password}
-              setPassword={setPassword}
-              handleLogin={handleLogin}
-              handleSignUp={handleSignUp}
-              hasAccount={hasAccount}
-              setHasAccount={setHasAccount}
-              emailError={emailError}
-              passwordError={passwordError}
-            />
-          </SignInPageStyles>
-        )}
-      </div>
-    </>
+    <Router>
+      <NavbarLinks>
+        <Switch>
+          <Route>
+            <Link to="contact" className="border">
+              Contact Us
+            </Link>
+          </Route>
+        </Switch>
+        <Switch>
+          <Route>
+            <Link to="login">Log in</Link>
+          </Route>
+        </Switch>
+      </NavbarLinks>
+      <NavbarLogo>
+        <Link to="/">
+          <Logo src="logo.png" />
+        </Link>
+        <div
+          style={{
+            marginTop: "30px",
+            margin: "auto",
+            fontWeight: "900",
+            fontFamily: "Josefin Sans, sans-serif",
+          }}
+        >
+          “A budget doesn’t limit your freedom; it gives you freedom.” – Rachel
+          Cruze
+        </div>
+      </NavbarLogo>
+
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+
+      <Route path="/contact">
+        <Contact />
+      </Route>
+
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
   );
 };
 
 export default App;
+
+// const SignInPageStyles = styled.div`
+//   position: absolute;
+//   top: 0;
+//   bottom: 0;
+//   left: 0;
+//   right: 0;
+//   background-color: #20bf55;
+//   background-image: linear-gradient(315deg, #20bf55 0%, #01baef 74%);
+//   overflow-y: auto;
+// `;
+
+// const App = () => {
+//   const [userId, setUserId] = useState('');
+//   const [user, setUser] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [emailError, setEmailError] = useState('');
+//   const [passwordError, setPasswordError] = useState('');
+//   const [hasAccount, setHasAccount] = useState(false);
+
+//   const clearInputs = () => {
+//     setEmail('');
+//     setPassword('');
+//   };
+
+//   const clearErrors = () => {
+//     setEmailError('');
+//     setPasswordError('');
+//   };
+
+//   const handleLogin = () => {
+//     clearErrors();
+//     fire
+//       .auth()
+//       .signInWithEmailAndPassword(email, password)
+//       .catch((err) => {
+//         switch (err.code) {
+//           case 'auth/invalid-email':
+//             setEmailError('Invalid Email');
+//             break;
+//           case 'auth/user-not-found':
+//             setEmailError('User not found');
+//             break;
+//           case 'auth/wrong-password':
+//             setPasswordError('Wrong password');
+//             break;
+//         }
+//       });
+//   };
+
+//   const handleSignUp = () => {
+//     clearErrors();
+//     fire
+//       .auth()
+//       .createUserWithEmailAndPassword(email, password)
+//       .catch((err) => {
+//         switch (err.code) {
+//           case 'auth/email-already-in-use':
+//             setEmailError(err.message);
+//             break;
+//           case 'auth/invalid-email':
+//             setEmailError('Invalid Email');
+//             break;
+//           case 'auth/weak-password':
+//             setPasswordError(err.message);
+//             break;
+//         }
+//       });
+//   };
+
+//   const handleLogout = () => {
+//     fire.auth().signOut();
+//   };
+
+//   const authListener = () => {
+//     fire.auth().onAuthStateChanged((user) => {
+//       if (user) {
+//         clearInputs();
+//         setUserId(user.uid);
+//         setUser(user);
+//       } else {
+//         setUser('');
+//       }
+//     });
+//   };
+
+//   useEffect(() => {
+//     authListener();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="App">
+//         {user ? (
+//           <HomePage
+//           userId={userId}
+//           handleLogout={handleLogout}
+//           />
+//         ) : (
+//           <SignInPageStyles>
+//             <SignInOrSignUp
+//               email={email}
+//               setEmail={setEmail}
+//               password={password}
+//               setPassword={setPassword}
+//               handleLogin={handleLogin}
+//               handleSignUp={handleSignUp}
+//               hasAccount={hasAccount}
+//               setHasAccount={setHasAccount}
+//               emailError={emailError}
+//               passwordError={passwordError}
+//             />
+//           </SignInPageStyles>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,173 +1,179 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import fire from '../fire';
-import Categories from './Categories';
-import Modal from 'react-modal';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import fire from "../fire";
+import Categories from "./Categories";
+import Modal from "react-modal";
 
-const CardsContainer = styled.div`
-  display: grid;
-  grid-template-columns: 3fr 3fr;
-  grid-gap: 25px 40px;
-  padding: 10px;
-`;
-
-const HomePageContainer = styled.div`
-  min-width: 570px;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #20bf55;
-  background-image: linear-gradient(315deg, #20bf55 0%, #01baef 74%);
-  overflow-y: auto;
-`;
-
-const ConfirmLogout = styled.button`
-    cursor: pointer;
-    height: 45px;
-    background-color: #FFC43D;
-    border: transparent;
-    width: 90px;
-    border-radius: 20px;
-`;
-
-const CancelLogout = styled.button`
-  background-color: #DCE0D9;
-  border: transparent; 
-  width: 90px;
-  borderRadius: 20px;
-  cursor: pointer;
-  height: 45px;
-  border-radius: 20px;
-`;
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    borderRadius: '20px',
-    border: 'none',
-    padding: '100px'
-  },
-};
-
-const AddCategoryPosition = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 150px;
-  margin: auto;
-  gap: 10px;
-  padding: 40px 0px;
-`;
-
-const LogoutButtonStyles = styled.button`
-margin-left: 20px; 
-margin-top: 20px; 
-background-color: #FFC43D;
-border: none; 
-border-radius: 5px; 
-cursor: pointer;
-height: 30px;
-width: 150px;
-`;
-
-const HomePage = ({ handleLogout, userId }) => {
-  const [newCategory, setNewCategory] = useState('');
-  const [categoryList, setCategoryList] = useState([]);
-  const [deleteId, setDeleteId] = useState('');
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [emptyCategory, setEmptyCategory] = useState(false);
-
-  const handleNewCategory = () => {
-    if (newCategory === ''){
-      setEmptyCategory(true);
-    } else {
-      const categoriesRef = fire.database().ref(`${userId}/categories`);
-      const newCat = {
-        newCategory,
-      };
-      const newPush = categoriesRef.push(newCat);
-      const newId = newPush.key;
-      setDeleteId(newId);
-      setNewCategory('');
-      location.reload();
-    }
-  };
-
-  useEffect(() => {
-    const categoriesRef = fire.database().ref(`${userId}/categories`);
-    categoriesRef.on('value', (snapshot) => {
-      const categories = snapshot.val();
-      const categoryList = [];
-      for (const id in categories) {
-        categoryList.push({id, ...categories[id]});
-      }
-      setCategoryList(categoryList);
-    });
-  },[]);
-
-  const openModal = () => {
-    setIsOpen(true);
-  }
-
-  const closeModal = () => {
-    setIsOpen(false);
-  }
-
-  const handleAddCategoryEnterKey = (e) => {
-    if (e.key === 'Enter'){
-      handleNewCategory();
-    }
-  }
-
-  return (
-    <HomePageContainer>
-      <LogoutButtonStyles onClick={openModal}>Logout</LogoutButtonStyles>
-      <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-      <h1 style={{ textAlign: 'center', color: '#FFC43D', fontSize: '50px' }}>BUDGETSTASH</h1>
-      <AddCategoryPosition>
-      <input 
-        type="text" 
-        placeholder="New Category" 
-        value={newCategory} 
-        onChange={(e) => setNewCategory(e.target.value)} 
-        onKeyDown={handleAddCategoryEnterKey}
-        style={{ borderRadius: '5px', border: 'none', height: '30px' }}
-      />
-      { emptyCategory ? (<div style={{ color: 'black', fontSize: '10px', marginTop: '-11px' }}>*Please enter a new category</div>) : (<></>)}
-      <button 
-      onClick={handleNewCategory}
-      style={{ borderRadius: '5px', border: 'none', height: '30px', backgroundColor: '#FFC43D', cursor: 'pointer' }}
-      >
-        Add Category
-      </button>
-      </AddCategoryPosition>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <CardsContainer>{categoryList.length !== 0 ? categoryList.map((categoryValue, index) => <Categories 
-        key={index} categoryValue={categoryValue} deleteId={deleteId} userId={userId}
-        />) : <h1 style={{ position: 'absolute', marginLeft: '-122px' }} >Create a new category!</h1>}</CardsContainer>
-      </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        ariaHideApp={false}
-        >
-          <div style={{ marginBottom: '25px', fontSize: '20px' }}>Are you sure you want to logout?</div>
-          <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-          <ConfirmLogout onClick={handleLogout}>YES</ConfirmLogout>
-          <CancelLogout onClick={closeModal}>NO</CancelLogout>
-          </div>
-      </Modal>
-      <div style={{ paddingBottom: '50px' }}/>
-    </HomePageContainer>
-  );
+const HomePage = () => {
+  return <div>Home page</div>;
 };
 
 export default HomePage;
+
+// const CardsContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: 3fr 3fr;
+//   grid-gap: 25px 40px;
+//   padding: 10px;
+// `;
+
+// const HomePageContainer = styled.div`
+//   min-width: 570px;
+//   position: absolute;
+//   top: 0;
+//   bottom: 0;
+//   left: 0;
+//   right: 0;
+//   background-color: #20bf55;
+//   background-image: linear-gradient(315deg, #20bf55 0%, #01baef 74%);
+//   overflow-y: auto;
+// `;
+
+// const ConfirmLogout = styled.button`
+//     cursor: pointer;
+//     height: 45px;
+//     background-color: #FFC43D;
+//     border: transparent;
+//     width: 90px;
+//     border-radius: 20px;
+// `;
+
+// const CancelLogout = styled.button`
+//   background-color: #DCE0D9;
+//   border: transparent;
+//   width: 90px;
+//   borderRadius: 20px;
+//   cursor: pointer;
+//   height: 45px;
+//   border-radius: 20px;
+// `;
+
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//     borderRadius: '20px',
+//     border: 'none',
+//     padding: '100px'
+//   },
+// };
+
+// const AddCategoryPosition = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   flex-direction: column;
+//   width: 150px;
+//   margin: auto;
+//   gap: 10px;
+//   padding: 40px 0px;
+// `;
+
+// const LogoutButtonStyles = styled.button`
+// margin-left: 20px;
+// margin-top: 20px;
+// background-color: #FFC43D;
+// border: none;
+// border-radius: 5px;
+// cursor: pointer;
+// height: 30px;
+// width: 150px;
+// `;
+
+// const HomePage = ({ handleLogout, userId }) => {
+//   const [newCategory, setNewCategory] = useState('');
+//   const [categoryList, setCategoryList] = useState([]);
+//   const [deleteId, setDeleteId] = useState('');
+//   const [modalIsOpen, setIsOpen] = useState(false);
+//   const [emptyCategory, setEmptyCategory] = useState(false);
+
+//   const handleNewCategory = () => {
+//     if (newCategory === ''){
+//       setEmptyCategory(true);
+//     } else {
+//       const categoriesRef = fire.database().ref(`${userId}/categories`);
+//       const newCat = {
+//         newCategory,
+//       };
+//       const newPush = categoriesRef.push(newCat);
+//       const newId = newPush.key;
+//       setDeleteId(newId);
+//       setNewCategory('');
+//       location.reload();
+//     }
+//   };
+
+//   useEffect(() => {
+//     const categoriesRef = fire.database().ref(`${userId}/categories`);
+//     categoriesRef.on('value', (snapshot) => {
+//       const categories = snapshot.val();
+//       const categoryList = [];
+//       for (const id in categories) {
+//         categoryList.push({id, ...categories[id]});
+//       }
+//       setCategoryList(categoryList);
+//     });
+//   },[]);
+
+//   const openModal = () => {
+//     setIsOpen(true);
+//   }
+
+//   const closeModal = () => {
+//     setIsOpen(false);
+//   }
+
+//   const handleAddCategoryEnterKey = (e) => {
+//     if (e.key === 'Enter'){
+//       handleNewCategory();
+//     }
+//   }
+
+//   return (
+//     <HomePageContainer>
+//       <LogoutButtonStyles onClick={openModal}>Logout</LogoutButtonStyles>
+//       <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+//       <h1 style={{ textAlign: 'center', color: '#FFC43D', fontSize: '50px' }}>BUDGETSTASH</h1>
+//       <AddCategoryPosition>
+//       <input
+//         type="text"
+//         placeholder="New Category"
+//         value={newCategory}
+//         onChange={(e) => setNewCategory(e.target.value)}
+//         onKeyDown={handleAddCategoryEnterKey}
+//         style={{ borderRadius: '5px', border: 'none', height: '30px' }}
+//       />
+//       { emptyCategory ? (<div style={{ color: 'black', fontSize: '10px', marginTop: '-11px' }}>*Please enter a new category</div>) : (<></>)}
+//       <button
+//       onClick={handleNewCategory}
+//       style={{ borderRadius: '5px', border: 'none', height: '30px', backgroundColor: '#FFC43D', cursor: 'pointer' }}
+//       >
+//         Add Category
+//       </button>
+//       </AddCategoryPosition>
+//       <div style={{ display: 'flex', justifyContent: 'center' }}>
+//         <CardsContainer>{categoryList.length !== 0 ? categoryList.map((categoryValue, index) => <Categories
+//         key={index} categoryValue={categoryValue} deleteId={deleteId} userId={userId}
+//         />) : <h1 style={{ position: 'absolute', marginLeft: '-122px' }} >Create a new category!</h1>}</CardsContainer>
+//       </div>
+//       <Modal
+//         isOpen={modalIsOpen}
+//         onRequestClose={closeModal}
+//         style={customStyles}
+//         ariaHideApp={false}
+//         >
+//           <div style={{ marginBottom: '25px', fontSize: '20px' }}>Are you sure you want to logout?</div>
+//           <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+//           <ConfirmLogout onClick={handleLogout}>YES</ConfirmLogout>
+//           <CancelLogout onClick={closeModal}>NO</CancelLogout>
+//           </div>
+//       </Modal>
+//       <div style={{ paddingBottom: '50px' }}/>
+//     </HomePageContainer>
+//   );
+// };
+
+// export default HomePage;
