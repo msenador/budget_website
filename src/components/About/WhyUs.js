408fb59ccd0809bebe858d2d0e637c73ed28eae1
Morian Senador <smorian@yahoo.com>
2021-11-27 19:26:32 -0500

    whyus styles

--- src/components/About/WhyUs.js
+++ src/components/About/WhyUs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
 
 const Container = styled.div``;
 
@@ -10,19 +11,34 @@ const Header = styled.h1`
   align-items: center;
   display: flex;
   justify-content: center;
+  &.phone {
+    font-size: 30px;
+  }
 `;
 
 const Content = styled.div`
   display: grid;
   grid-template-columns: 3fr 3fr 3fr;
-  grid-gap: 25px 40px;
+  text-align: center;
 `;
 
 const WhyUs = () => {
+  const mobileTablet = useMediaQuery({
+    query: "(min-width: 541px)",
+  });
+  const laptopOrDesktop = useMediaQuery({
+    query: "(min-width: 1025px)",
+  });
   return (
     <Container>
-      <Header>What We Offer</Header>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <Header
+        className={
+          laptopOrDesktop ? "desktop" : mobileTablet ? "tablet" : "phone"
+        }
+      >
+        What We Offer
+      </Header>
+      <div>
         <Content>
           <div>Price (FREE) </div>
           <div>security.</div>
